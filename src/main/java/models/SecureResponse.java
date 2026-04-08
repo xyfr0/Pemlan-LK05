491eaba698789fac618b61d39744f6515b5c6c46
xyfr0 <jabbarirhamabdul0@gmail.com>
2026-04-08 10:23:42 +0700

    fill SecureResponse

--- src/main/java/models/SecureResponse.java
+++ src/main/java/models/SecureResponse.java
@@ -1,4 +1,13 @@
 package models;
 
 public class SecureResponse<T>{
+    private boolean success;
+    private T data;
+    private String warningMessage;
+
+    public SecureResponse(boolean success, T data, String warningMessage) {
+        this.success = success;
+        this.data = data;
+        this.warningMessage = warningMessage;
+    }
 }
